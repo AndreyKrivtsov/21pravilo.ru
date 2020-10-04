3b50fe83f01f0d3d564d31e0a14d6c57e8101c9b
AK <andreykrivtsov@gmail.com>
2020-10-05 01:51:36 +0300

    develop

---
+++ app/core/Db.js
@@ -0,0 +1,34 @@
+import Mongo from 'mongodb'
+
+let instance = null
+
+class Db {
+    constructor() {
+        if (instance) {
+            return instance
+        }
+        this.db = null
+        this.statusConnect = false
+        this.connect(Mongo.MongoClient)
+
+        instance = this
+    }
+
+    connect(client) {
+        if (!this.statusConnect) {
+            client.connect('mongodb://127.0.0.1:27017/', {
+                useNewUrlParser: true,
+                auth: {username: "root", password: "Toor753"},
+              }, (err, db) => {
+                if (err) {
+                    throw err
+                }
+                console.log('[mongodb] Connected to MongoDb')
+                this.statusConnect = true
+                this.db = db
+            })
+        }
+    }
+}
+
+export default Db
---
+++ app/models/BaseModel.js
@@ -0,0 +1,9 @@
+import Db from './../core/Db.js'
+
+class BaseModel {
+    constructor(client) {
+        this.db = new Db().db.db('21pravilo')
+    }
+}
+
+export default BaseModel
