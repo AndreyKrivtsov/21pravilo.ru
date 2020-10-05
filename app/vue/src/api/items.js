cc7bd390751ca06e0f51d256be071869cb088876
AK <andreykrivtsov@gmail.com>
2020-10-06 02:46:04 +0300

    feature-env

--- app/vue/src/api/items.js
+++ app/vue/src/api/items.js
@@ -2,22 +2,22 @@ import http from '../plugins/Http'
 
 export default {
     async getTopItems() {
-        return await http('http://localhost/api/items', 'GET')
+        return await http(window.config.apiUrl + '/api/items', 'GET')
     },
 
     async setItem(item) {
-        return await http('http://localhost/api/items', 'PUT', item)
+        return await http(window.config.apiUrl + '/api/items', 'PUT', item)
     },
 
     async getVoteItems() {
-        return await http('http://localhost/api/voteitems', 'GET')
+        return await http(window.config.apiUrl + '/api/voteitems', 'GET')
     },
 
     async setLike(data) {
-        return await http('http://localhost/api/voteitems/like', 'PUT', data)
+        return await http(window.config.apiUrl + '/api/voteitems/like', 'PUT', data)
     },
 
     async setDislike(data) {
-        return await http('http://localhost/api/voteitems/dislike', 'PUT', data)
+        return await http(window.config.apiUrl + '/api/voteitems/dislike', 'PUT', data)
     },
 }
