8863e10958bba4f864035a7cb8a478b1d0387f2e
AK <andreykrivtsov@gmail.com>
2020-10-07 02:17:17 +0300

    features

--- app/vue/src/pages/vote.vue
+++ app/vue/src/pages/vote.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h3 class="mt-4">Держите {{ randomList.length }} случайных правил</h3>
+        <h3 class="mt-4">{{ randomList.length }} случайных правил</h3>
 
         <div class="message-wrapper">
             <div class="message" v-if="timerMs > 0">
@@ -10,23 +10,28 @@
         </div>
 
         <div>
-            <div v-for="item in randomList" :key="item" class="list">
+            <div v-for="(item, index) in randomList" :key="index" class="list">
                 <span>{{ item.name }}</span>
                 <div class="text-right">
-                    <button
-                        :disabled="block"
-                        @click="like(item)"
-                        class="btn btn-primary"
-                    >
-                        Like
-                    </button>
-                    <button
-                        :disabled="block"
-                        @click="dislike(item)"
-                        class="btn btn-primary"
-                    >
-                        Dislike
-                    </button>
+                    <template v-if="!voted[index]">
+                        <button
+                            :disabled="block"
+                            @click="like(item, index)"
+                            class="btn btn-primary"
+                        >
+                            Like
+                        </button>
+                        <button
+                            :disabled="block"
+                            @click="dislike(item, index)"
+                            class="btn btn-primary"
+                        >
+                            Dislike
+                        </button>
+                    </template>
+                    <template v-else>
+                        Голос учтен
+                    </template>
                 </div>
             </div>
         </div>
@@ -34,7 +39,7 @@
 </template>
 
 <script>
-import ItemsApi from "./../api/items"
+import ItemsApi from "./../api/items";
 
 export default {
     name: "Vote",
@@ -45,41 +50,44 @@ export default {
             token: String,
             timerMs: 0,
             block: true,
+            voted: [],
         };
     },
     methods: {
-        like(item) {
-            ItemsApi.setLike({ id: item._id, token: this.token })
+        like(item, index) {
+            this.voted[index] = true;
+            ItemsApi.setLike({ id: item._id, token: this.token });
         },
 
-        dislike(item) {
-            ItemsApi.setDislike({ id: item._id, token: this.token })
+        dislike(item, index) {
+            this.voted[index] = true;
+            ItemsApi.setDislike({ id: item._id, token: this.token });
         },
 
         getVoteItems() {
             ItemsApi.getVoteItems().then((data) => {
-                this.randomList = data.items
-                this.token = data.token
-                this.runTimeout(data.timeout)
+                this.randomList = data.items;
+                this.token = data.token;
+                this.runTimeout(data.timeout);
             });
         },
 
         runTimeout(timeout) {
-            this.timerMs -= 1000
-            this.timerMs = timeout
+            this.timerMs -= 1000;
+            this.timerMs = timeout;
             let interval = setInterval(() => {
                 if (this.timerMs > 1000) {
-                    this.timerMs -= 1000
+                    this.timerMs -= 1000;
                 } else {
-                    this.timerMs = 0
-                    this.block = false
-                    clearInterval(interval)
+                    this.timerMs = 0;
+                    this.block = false;
+                    clearInterval(interval);
                 }
-            }, 1000)
+            }, 1000);
         },
     },
     mounted() {
-        this.getVoteItems()
+        this.getVoteItems();
     },
 };
 </script>
@@ -97,7 +105,7 @@ export default {
         padding: 5px;
         width: 100%;
         height: 100%;
-        background-color: #EEE;
+        background-color: #eee;
     }
 }
 </style>
